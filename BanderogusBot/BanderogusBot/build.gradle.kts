0b5f10e05da1f504b89b841b818fef1338463d1f
Hoker <74815861+HOKER1597@users.noreply.github.com>
2025-02-11 12:58:13 +0200

    Add files via upload

---
+++ BanderogusBot/BanderogusBot/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.telegram/telegrambots
+    implementation("org.telegram:telegrambots:6.3.0")
+    implementation ("org.slf4j:slf4j-simple:2.0.7") // або інший логер
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
